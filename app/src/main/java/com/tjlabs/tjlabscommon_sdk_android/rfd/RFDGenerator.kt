4197215561136e74ab537d2ccc1127010473ccd4
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-01-03 11:09:45 +0900

    feat:func input & output

--- app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/rfd/RFDGenerator.kt
+++ app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/rfd/RFDGenerator.kt
@@ -1,31 +1,44 @@
 package com.tjlabs.tjlabscommon_sdk_android.rfd
+import android.os.Handler
+import android.os.Looper
 
-import com.tjlabs.tjlabscommon_sdk_android.utils.TJLabsUtilFunctions.getCurrentTimeInMilliseconds
-
-class RFDGenerator {
-    // Timer 관련 변수
-    interface RFDGeneratorListener {
-        fun onRFDResult(rfd: ReceivedForce)
+class RFDGenerator() {
+    interface RFDCallback {
+        fun onRFDResult(rfdList: List<ReceivedForce>)
     }
 
-    var rfdListener: RFDGeneratorListener? = null
+    private val handler = Handler(Looper.getMainLooper())
+    private var timerRunnable: Runnable? = null
 
+    val inputReceivedForce = mutableListOf<ReceivedForce>()
+    var count = 0
 
     fun generateRFD(
+        rfdLength : Int,
         intervalMillis: Long,
         id: String = "",
-        bleScanInfoSet: MutableSet<BLEScanInfo>,
-        pressure: Float = 0f
+        getBleScanInfoSet: () -> MutableSet<BLEScanInfo>, // 최신 데이터 참조를 위한 람다
+        pressure: Float = 0f,
+        callback: RFDCallback
     ) {
-        val averageBleMap = TJLabsBluetoothFunctions.averageBLEScanInfoSet(bleScanInfoSet)
-        rfdListener?.onRFDResult(
-            ReceivedForce(
-                id,
-                getCurrentTimeInMilliseconds() - (intervalMillis / 2),
-                averageBleMap,
-                pressure
-            )
-        )
+        timerRunnable = object : Runnable {
+            override fun run() {
+                val currentBleScanInfoSet = getBleScanInfoSet()
+                val averageBleMap = TJLabsBluetoothFunctions.averageBLEScanInfoSet(currentBleScanInfoSet)
+                if (count < rfdLength) {
+                    inputReceivedForce.add(
+                    ReceivedForce(id, System.currentTimeMillis(), averageBleMap, pressure))
+                    count++
+                }
+                if (count == rfdLength) {
+                    callback.onRFDResult(inputReceivedForce) // 결과 리턴
+                    count = 0
+                    inputReceivedForce.clear()
+                }
+                handler.postDelayed(this, intervalMillis)
+            }
+        }
+        handler.postDelayed(timerRunnable!!, intervalMillis)
     }
 
 
