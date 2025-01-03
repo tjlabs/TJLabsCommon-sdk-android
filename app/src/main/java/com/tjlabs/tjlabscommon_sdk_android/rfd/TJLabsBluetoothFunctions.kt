adfffcfa07fb579f77ef791a70acb816b894838e
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-01-03 11:14:55 +0900

    rename

--- app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/rfd/TJLabsBluetoothFunctions.kt
+++ app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/rfd/TJLabsBluetoothFunctions.kt
@@ -30,7 +30,7 @@ object TJLabsBluetoothFunctions {
         return averageMap
     }
 
-    internal  fun checkBleChannelNum(bleMap: Map<String, Float>?, threshold : Float = -95f): Int {
+    internal fun checkBLEChannelNum(bleMap: Map<String, Float>?, threshold : Float = -95f): Int {
         var numChannels = 0
         bleMap?.forEach { (key, value) ->
             val bleRssi = value ?: -100.0f
