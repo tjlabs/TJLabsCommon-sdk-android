c81faa4f50d8d78c37d62dfeec57ac978f7e8c02
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-01-03 13:55:37 +0900

    feat:RFDFunction
    - checkBLEChannelNum

---
+++ app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/rfd/RFDFunctions.kt
@@ -0,0 +1,14 @@
+package com.tjlabs.tjlabscommon_sdk_android.rfd
+
+class RFDFunctions {
+    fun checkBLEChannelNum(bleMap: Map<String, Float>?, threshold : Float = -95f): Int {
+        var numChannels = 0
+        bleMap?.forEach { (key, value) ->
+            val bleRssi = value ?: -100.0f
+            if (bleRssi > threshold) {
+                numChannels++
+            }
+        }
+        return numChannels
+    }
+}
