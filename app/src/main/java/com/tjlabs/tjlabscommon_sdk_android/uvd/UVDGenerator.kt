d10a51a1bfeb3e9758a66d2908c585b3a4ebb4db
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-01-10 10:40:16 +0900

    feat:UVDGenerator
    - del frequency input
    - feat callback interface
    - generatePedstrianUvd
    - zeroVelocityAfterSeconds

--- app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/uvd/UVDGenerator.kt
+++ app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/uvd/UVDGenerator.kt
@@ -1,35 +1,98 @@
 package com.tjlabs.tjlabscommon_sdk_android.uvd
 
 import android.app.Application
-import android.util.Log
-import com.tjlabs.tjlabscommon_sdk_android.rfd.ReceivedForce
-import com.tjlabs.tjlabscommon_sdk_android.rfd.ScanMode
+import com.tjlabs.tjlabscommon_sdk_android.uvd.pdr.TJLabsPDRDistanceEstimator
 
-class UVDGenerator(application: Application, val userId : String = "") {
+class UVDGenerator(application: Application, private val userId : String = "") {
     interface UVDCallback {
-        fun onUvdResult(success : Boolean, msg : String, uvd: ReceivedForce)
+        fun onUvdResult(mode : UserMode, uvd: UserVelocity)
+
+        fun onVelocityResult(kmPh : Float)
+
+        fun onUvdPauseMillis(time : Long)
+
+        fun onUvdError(error : String)
     }
 
     private val tjLabsSensorManager : TJLabsSensorManager = TJLabsSensorManager(application)
+    private var tjLabsPdrDistanceEstimator : TJLabsPDRDistanceEstimator = TJLabsPDRDistanceEstimator()
+    private var tjLabsAttitudeEstimator : TJLabsAttitudeEstimator = TJLabsAttitudeEstimator()
+    private var tjLabsUnitStatusEstimator = TJLabsUnitStatusEstimator()
+    private var uvdGenerationTimeMillis = 0L
+    private var userMode = UserMode.MODE_PEDESTRIAN
+    private var drVelocityScale = 1f
 
-
-    fun setUserMode(userMode: ScanMode) {
-
+    fun setUserMode(mode: UserMode) {
+        userMode = mode
     }
 
-    fun generateUvd(sensorFrequency : Int = 40) {
+    fun updateDrVelocityScale(scale : Float) {
+        TODO()
+    }
+
+    fun generateUvd(defaultPDRStepLength: Float = tjLabsPdrDistanceEstimator.getDefaultStepLength(),
+                    minPDRStepLength : Float = tjLabsPdrDistanceEstimator.getMinStepLength(),
+                    maxPDRStepLength : Float = tjLabsPdrDistanceEstimator.getMaxStepLength(),
+                    callback : UVDCallback) {
         val (isCheckSensorSuccess, msgCheckSensor) = tjLabsSensorManager.checkSensorAvailability()
         if (isCheckSensorSuccess) {
-            tjLabsSensorManager.setSensorFrequency(sensorFrequency)
+            uvdGenerationTimeMillis = System.currentTimeMillis()
+            tjLabsPdrDistanceEstimator.setDefaultStepLength(defaultPDRStepLength)
+            tjLabsPdrDistanceEstimator.setMinStepLength(minPDRStepLength)
+            tjLabsPdrDistanceEstimator.setMaxStepLength(maxPDRStepLength)
             tjLabsSensorManager.getSensorDataResultOrNull(object : TJLabsSensorManager.SensorResultListener{
                 override fun onSensorChangedResult(sensorData: SensorData) {
-                    Log.d("onSensorChanged", sensorData.toString())
+                    when (userMode) {
+                        UserMode.MODE_PEDESTRIAN -> generatePedestrianUvd(sensorData, callback)
+                        UserMode.MODE_VEHICLE -> TODO()
+                        UserMode.MODE_AUTO -> TODO()
+                    }
                 }
             })
+        } else {
+            callback.onUvdError(msgCheckSensor)
         }
+    }
+
+    private fun generatePedestrianUvd(sensorData: SensorData, callback: UVDCallback) {
+        val pdrUnit = tjLabsPdrDistanceEstimator.estimateDistanceInfo(System.currentTimeMillis(), sensorData)
+        val attDegree = tjLabsAttitudeEstimator.estimateAttitudeRadian(System.currentTimeMillis(), sensorData).toDegree()
+        val isLookingStatus = tjLabsUnitStatusEstimator.estimateStatus(attDegree, pdrUnit.isIndexChanged)
+        if (pdrUnit.isIndexChanged) {
+            val index = pdrUnit.index
+            val length = pdrUnit.length
+            val heading = attDegree.yaw
+            callback.onUvdResult(UserMode.MODE_PEDESTRIAN, UserVelocity(userId, System.currentTimeMillis(), index, length, heading, isLookingStatus))
+            uvdGenerationTimeMillis = System.currentTimeMillis()
+        } else {
+            callback.onUvdPauseMillis(System.currentTimeMillis() - uvdGenerationTimeMillis)
+        }
+        callback.onVelocityResult(zeroVelocityAfterSeconds(pdrUnit.velocity))
+    }
+
+    private fun zeroVelocityAfterSeconds(velocity : Float, sec : Int = 2) : Float {
+        return if (System.currentTimeMillis() - uvdGenerationTimeMillis < sec * 1000) {
+            velocity
+        } else {
+            0f
+        }
+    }
+
+    private fun generateVehicleUvd() {
+        TODO()
+    }
+
+    private fun generateAutoUvd() {
+        TODO()
     }
 
     fun stopUvdGeneration() {
         tjLabsSensorManager.stopSensorChanged()
+        tjLabsPdrDistanceEstimator = TJLabsPDRDistanceEstimator()
+        tjLabsAttitudeEstimator = TJLabsAttitudeEstimator()
+        tjLabsUnitStatusEstimator = TJLabsUnitStatusEstimator()
+        uvdGenerationTimeMillis = 0L
+        userMode = UserMode.MODE_PEDESTRIAN
+        drVelocityScale = 1f
     }
 }
