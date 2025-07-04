5843481384588b5d326a4a727ccdf14e2c8f5b43
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-07-04 15:17:00 +0900

    release 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val versionMajor = 1
 val versionMinor = 0
-val versionPatch = 1
+val versionPatch = 2
 
 
 android {
@@ -35,7 +35,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
