e651cf56f3d1f739f3d2b938300035f7bb04620e
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-09-18 19:23:00 +0900

    Update UVDGenerator.kt

--- app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/uvd/UVDGenerator.kt
+++ app/src/main/java/com/tjlabs/tjlabscommon_sdk_android/uvd/UVDGenerator.kt
@@ -1,6 +1,7 @@
 package com.tjlabs.tjlabscommon_sdk_android.uvd
 
 import android.app.Application
+import android.util.Log
 import com.tjlabs.tjlabscommon_sdk_android.simulation.JupiterSimulator
 import com.tjlabs.tjlabscommon_sdk_android.simulation.JupiterSimulator.convertToSensorData
 import com.tjlabs.tjlabscommon_sdk_android.simulation.JupiterSimulator.saveDataFunction
@@ -11,6 +12,10 @@ import com.tjlabs.tjlabscommon_sdk_android.uvd.pdr.TJLabsPDRDistanceEstimator
 
 
 const val sensorFrequency = 40
+const val MODE_CHANGE_TIME_CONDITION: Float = 10 * 1000f
+const val MODE_CHANGE_RFLOW_TIME_OVER: Float = 0.1f
+const val MODE_CHANGE_RFLOW_FORCE: Float = 0.065f
+const val MODE_CHANGE_TIME_AFTER_ROUTE_TRACK: Float = 30 * 1000f
 
 class UVDGenerator(private val application: Application, private val userId : String = "") {
     interface UVDCallback {
@@ -31,13 +36,72 @@ class UVDGenerator(private val application: Application, private val userId : St
     private var tjLabsPdrDistanceEstimator : TJLabsPDRDistanceEstimator = TJLabsPDRDistanceEstimator()
     private var tjLabsDrDistanceEstimator : TJLabsDRDistanceEstimator = TJLabsDRDistanceEstimator()
     private var tjLabsUnitStatusEstimator = TJLabsUnitStatusEstimator()
+
     private var uvdGenerationTimeMillis = 0L
-    private var userMode = UserMode.MODE_PEDESTRIAN
+
+    private var setUserMode = UserMode.MODE_PEDESTRIAN // UVD Generator 동작을 위한 설정
+    private var currentUserMode = UserMode.MODE_VEHICLE // AutoMode 동작 내 판단을 위한 설정
+    private var preUserMode = UserMode.MODE_VEHICLE
     private var drVelocityScale = 1f
 
+    private var lastModeChangedTime: Long = 0
+    private var lastStepChangedTime: Long = 0
     private var preTime = 0L
+
+    private var autoUnitIndexCount = 0
+
+    private var rflow: Float = 0.0f
+    private var rflowForVelocity: Float = 0.0f
+    private var rflowForAutoMode: Float = 0.0f
+
+    private var isSufficientRfdBuffer: Boolean = false
+    private var isSufficientRfdVelocityBuffer: Boolean = false
+    private var isSufficientRfdAutoMode: Boolean = false
+
+    private var routeTrackFinishedTime : Long = 0
+
+    private var isInEntranceLevel: Boolean = false
+    private var isStartRoutTrack: Boolean = false
+
+    private var isBackGround = false
+
+    fun setRFlow(rflow: Float, rflowForVelocity: Float, rflowForAutoMode: Float, isSufficient: Boolean, isSufficientForVelocity: Boolean, isSufficientForAutoMode: Boolean) {
+        this.rflow = rflow
+        this.rflowForVelocity = rflowForVelocity
+        this.rflowForAutoMode = rflowForAutoMode
+
+        isSufficientRfdBuffer = isSufficient
+        isSufficientRfdVelocityBuffer = isSufficientForVelocity
+        isSufficientRfdAutoMode = isSufficientForAutoMode
+
+        tjLabsDrDistanceEstimator.setRFlow(
+            rflow,
+            rflowForVelocity,
+            rflowForAutoMode,
+            isSufficient,
+            isSufficientForVelocity,
+            isSufficientForAutoMode
+        )
+    }
+
+    fun setRouteTrackFinishedTime(time : Long) {
+        routeTrackFinishedTime = time
+    }
+
+    fun setIsInEntranceLevel(flag : Boolean) {
+        isInEntranceLevel = flag
+    }
+
+    fun setIsStartRoutTrack(flag : Boolean) {
+        isStartRoutTrack = flag
+    }
+
+    fun setIsBackground(flag : Boolean) {
+        isBackGround = flag
+    }
+
     fun setUserMode(mode: UserMode) {
-        userMode = mode
+        setUserMode = mode
     }
 
     fun updateDrVelocityScale(scale : Float) {
@@ -70,10 +134,10 @@ class UVDGenerator(private val application: Application, private val userId : St
             override fun onSensorChangedResult(sensorData: SensorData) {
                 val curTime = System.currentTimeMillis()
                 val dtime = if (preTime != 0L) {curTime - preTime} else {null}
-                when (userMode) {
+                when (setUserMode) {
                     UserMode.MODE_PEDESTRIAN -> generatePedestrianUvd(curTime, dtime,sensorData, callback)
                     UserMode.MODE_VEHICLE -> generateVehicleUvd(curTime, dtime,sensorData, callback)
-                    UserMode.MODE_AUTO -> TODO()
+                    UserMode.MODE_AUTO -> generateAutoUvd(curTime, dtime, sensorData, callback)
                 }
 
                 saveDataFunction(application, isSaveData, fileName, "${curTime},${sensorData.toCollectString()}" + "\n")
@@ -106,10 +170,10 @@ class UVDGenerator(private val application: Application, private val userId : St
                     val curTime = System.currentTimeMillis()
                     val dtime = if (preTime != 0L) {simulationTime - preTime} else {null}
                     if (sensorSimulationIndex <= sensorMutableList.size) {
-                        when (userMode) {
+                        when (setUserMode) {
                             UserMode.MODE_PEDESTRIAN -> generatePedestrianUvd(curTime, dtime,simulationSensorData, callback)
                             UserMode.MODE_VEHICLE -> generateVehicleUvd(curTime, dtime,simulationSensorData, callback)
-                            UserMode.MODE_AUTO -> TODO()
+                            UserMode.MODE_AUTO -> generateAutoUvd(curTime, dtime, simulationSensorData, callback)
                         }
                     }else{
                         stopUvdGeneration()
@@ -135,6 +199,8 @@ class UVDGenerator(private val application: Application, private val userId : St
         val pdrUnit = tjLabsPdrDistanceEstimator.estimateDistanceInfo(time, sensorData)
         val attDegree = tjLabsAttitudeEstimator.estimateAttitudeRadian(dtime, sensorData).toDegree()
         val isLookingStatus = tjLabsUnitStatusEstimator.estimateStatus(attDegree, pdrUnit.isIndexChanged)
+        tjLabsPdrDistanceEstimator.setAutoMode(false)
+
         if (pdrUnit.isIndexChanged) {
             val index = pdrUnit.index
             val length = pdrUnit.length
@@ -151,8 +217,10 @@ class UVDGenerator(private val application: Application, private val userId : St
     private fun generateVehicleUvd(time : Long, dtime : Long?, sensorData: SensorData, callback: UVDCallback) {
         val (drUnit, magNormSmoothingVariance) = tjLabsDrDistanceEstimator.estimateDistanceInfo(dtime, sensorData)
         val attDegree = tjLabsAttitudeEstimator.estimateAttitudeRadian(dtime, sensorData).toDegree()
+
         //TODO() 자석 거치 상황인지 확인
         //TODO() calAccBias?
+
         if (drUnit.isIndexChanged) {
             val index = drUnit.index
             val length = drUnit.length
@@ -167,8 +235,95 @@ class UVDGenerator(private val application: Application, private val userId : St
         callback.onMagNormSmoothingVarResult(magNormSmoothingVariance)
     }
 
-    private fun generateAutoUvd() {
-        TODO()
+    private fun generateAutoUvd(time : Long, dtime : Long?, sensorData: SensorData, callback: UVDCallback) {
+        val pdrUnit = tjLabsPdrDistanceEstimator.estimateDistanceInfo(time, sensorData)
+        val attDegree = tjLabsAttitudeEstimator.estimateAttitudeRadian(dtime, sensorData).toDegree()
+        val (drUnit, magNormSmoothingVariance) = tjLabsDrDistanceEstimator.estimateDistanceInfo(dtime, sensorData)
+        val isLookingStatus = tjLabsUnitStatusEstimator.estimateStatus(attDegree, pdrUnit.isIndexChanged)
+
+        val currentTime = System.currentTimeMillis()
+
+        if (isBackGround) {
+            lastModeChangedTime = currentTime
+        }
+
+        val isNormalStep = tjLabsPdrDistanceEstimator.getNormalStepCountFlag()
+
+        if (currentTime - lastModeChangedTime >= MODE_CHANGE_TIME_CONDITION) {
+            if (currentUserMode == UserMode.MODE_VEHICLE && isNormalStep) {
+                currentUserMode = UserMode.MODE_PEDESTRIAN
+                lastModeChangedTime = currentTime
+            } else {
+                val diffTime = currentTime - lastStepChangedTime
+                if (isSufficientRfdAutoMode && diffTime >= MODE_CHANGE_TIME_CONDITION) {
+                    if (rflowForAutoMode < MODE_CHANGE_RFLOW_TIME_OVER) {
+                        currentUserMode = UserMode.MODE_VEHICLE
+                        lastModeChangedTime = currentTime
+                    }
+                } else if (isSufficientRfdAutoMode) {
+                    if (rflowForAutoMode < MODE_CHANGE_RFLOW_FORCE) {
+                        currentUserMode = UserMode.MODE_VEHICLE
+                        lastModeChangedTime = currentTime
+                    }
+                }
+            }
+
+            val diffRouteTrackTime = currentTime - routeTrackFinishedTime
+            if (isInEntranceLevel || isStartRoutTrack) {
+                currentUserMode = UserMode.MODE_VEHICLE
+                lastModeChangedTime = currentTime
+            } else if (diffRouteTrackTime > 0 && diffRouteTrackTime < MODE_CHANGE_TIME_AFTER_ROUTE_TRACK ) {
+                currentUserMode = UserMode.MODE_VEHICLE
+                lastModeChangedTime = currentTime
+            }
+        }
+
+        if (currentUserMode == UserMode.MODE_PEDESTRIAN) {
+            tjLabsPdrDistanceEstimator.setAutoMode(true)
+            if (pdrUnit.isIndexChanged) {
+                lastStepChangedTime = currentTime
+                autoUnitIndexCount += 1
+
+                val length = pdrUnit.length
+                val heading = attDegree.yaw
+
+                uvdGenerationTimeMillis = time
+
+                callback.onUvdResult(UserMode.MODE_PEDESTRIAN, UserVelocity(userId, time, autoUnitIndexCount, length, heading, isLookingStatus))
+            } else{
+                callback.onUvdPauseMillis(time - uvdGenerationTimeMillis)
+            }
+        } else {
+            if (drUnit.isIndexChanged) {
+                lastStepChangedTime = currentTime
+                autoUnitIndexCount += 1
+                val length = drUnit.length
+                val heading = attDegree.yaw
+
+                uvdGenerationTimeMillis = time
+                callback.onUvdResult(UserMode.MODE_VEHICLE, UserVelocity(userId, time, autoUnitIndexCount, length, heading, true))
+            } else{
+                callback.onUvdPauseMillis(time - uvdGenerationTimeMillis)
+            }
+        }
+
+        if (currentUserMode != preUserMode) {
+            if (currentUserMode == UserMode.MODE_PEDESTRIAN) {
+                tjLabsPdrDistanceEstimator.setModeDrToPdr(true)
+            } else {
+                tjLabsPdrDistanceEstimator.setModeDrToPdr(false)
+            }
+        }
+
+        preUserMode = currentUserMode
+
+        callback.onPressureResult(sensorData.pressure[0])
+        if (currentUserMode == UserMode.MODE_PEDESTRIAN) {
+            callback.onVelocityResult(resetVelocityAfterSeconds(pdrUnit.velocity))
+        } else {
+            callback.onVelocityResult(resetVelocityAfterSeconds(drUnit.velocity))
+        }
+        callback.onMagNormSmoothingVarResult(magNormSmoothingVariance)
     }
 
     fun stopUvdGeneration() {
